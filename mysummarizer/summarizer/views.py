9ddb08ac0f3a3d7c156b5f9d6ab77dfea82ff1ee
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-10-05 02:52:16 +0300

    index ve views güncellendi.

--- mysummarizer/summarizer/views.py
+++ mysummarizer/summarizer/views.py
@@ -11,96 +11,173 @@ import networkx as nx
 
 # Create your views here.
 
-def read_article(file_name):
-    file = open(file_name, "r",encoding="utf-8")
-    filedata = file.readlines()
-    article = filedata[0].split(". ")
-    sentences = []
+# def read_article(file_name):
+#     file = open(file_name, "r",encoding="utf-8")
+#     filedata = file.readlines()
+#     article = filedata[0].split(". ")
+#     sentences = []
 
-    for sentence in article:
-        print(sentence)
-        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
-    sentences.pop()
+#     for sentence in article:
+#         print(sentence)
+#         sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+#     sentences.pop()
 
-    return sentences
-
-
-def sentence_similarity(sent1, sent2, stopwords=None):
-    if stopwords is None:
-        stopwords = []
-
-    sent1 = [w.lower() for w in sent1]
-    sent2 = [w.lower() for w in sent2]
-
-    all_words = list(set(sent1 + sent2))
-
-    vector1 = [0] * len(all_words)
-    vector2 = [0] * len(all_words)
-
-    # build the vector for the first sentence
-    for w in sent1:
-        if w in stopwords:
-            continue
-        vector1[all_words.index(w)] += 1
-
-    # build the vector for the second sentence
-    for w in sent2:
-        if w in stopwords:
-            continue
-        vector2[all_words.index(w)] += 1
-
-    return 1 - cosine_distance(vector1, vector2)
+#     return sentences
 
 
-def build_similarity_matrix(sentences, stop_words):
-    # Create an empty similarity matrix
-    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+# def sentence_similarity(sent1, sent2, stopwords=None):
+#     if stopwords is None:
+#         stopwords = []
 
-    for idx1 in range(len(sentences)):
-        for idx2 in range(len(sentences)):
-            if idx1 == idx2:  # ignore if both are same sentences
-                continue
-            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+#     sent1 = [w.lower() for w in sent1]
+#     sent2 = [w.lower() for w in sent2]
 
-    return similarity_matrix
+#     all_words = list(set(sent1 + sent2))
 
+#     vector1 = [0] * len(all_words)
+#     vector2 = [0] * len(all_words)
 
-def generate_summary(file_name, top_n=5):
-    stop_words = stopwords.words('turkish')
-    summarize_text = []
+#     # build the vector for the first sentence
+#     for w in sent1:
+#         if w in stopwords:
+#             continue
+#         vector1[all_words.index(w)] += 1
 
-    # Step 1 - Read text anc split it
-    sentences = read_article(file_name)
+#     # build the vector for the second sentence
+#     for w in sent2:
+#         if w in stopwords:
+#             continue
+#         vector2[all_words.index(w)] += 1
 
-    # Step 2 - Generate Similary Martix across sentences
-    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
-
-    # Step 3 - Rank sentences in similarity martix
-    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
-    scores = nx.pagerank(sentence_similarity_graph)
-
-    # Step 4 - Sort the rank and pick top sentences
-    ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
-    print("Indexes of top ranked_sentence order are ", ranked_sentence)
-
-    for i in range(top_n):
-        summarize_text.append(" ".join(ranked_sentence[i][1]))
-
-    # Step 5 - Offcourse,
-    print("Summarize Text: \n", ". ".join(summarize_text))
+#     return 1 - cosine_distance(vector1, vector2)
 
 
-# let's begin
+# def build_similarity_matrix(sentences, stop_words):
+#     # Create an empty similarity matrix
+#     similarity_matrix = np.zeros((len(sentences), len(sentences)))
 
-read_article("deneme.txt")
+#     for idx1 in range(len(sentences)):
+#         for idx2 in range(len(sentences)):
+#             if idx1 == idx2:  # ignore if both are same sentences
+#                 continue
+#             similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
 
-generate_summary("deneme.txt", 2)
+#     return similarity_matrix
+
+
+# def generate_summary(file_name, top_n=5):
+#     stop_words = stopwords.words('turkish')
+#     summarize_text = []
+
+#     # Step 1 - Read text anc split it
+#     sentences = read_article(file_name)
+
+#     # Step 2 - Generate Similary Martix across sentences
+#     sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+#     # Step 3 - Rank sentences in similarity martix
+#     sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+#     scores = nx.pagerank(sentence_similarity_graph)
+
+#     # Step 4 - Sort the rank and pick top sentences
+#     ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+#     print("Indexes of top ranked_sentence order are ", ranked_sentence)
+
+#     for i in range(top_n):
+#         summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+#     # Step 5 - Offcourse,
+#     print("Summarize Text: \n", ". ".join(summarize_text))
+
+
+# # let's begin
+
+# read_article("deneme.txt")
+
+# generate_summary("deneme.txt", 2)
 
 def index(request):
+
+    ozet_metin = []
 
     if request.method == "POST":
         ceviri = request.POST["ceviri_metin"]
 
-        
+        article = ceviri.split(". ")
+        sentences = []
+        for sentence in article:
+            # print(sentence)
+            sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+        sentences.pop()
+        def sentence_similarity(sent1, sent2, stopwords=None):
+            if stopwords is None:
+                stopwords = []
+
+            sent1 = [w.lower() for w in sent1]
+            sent2 = [w.lower() for w in sent2]
+
+            all_words = list(set(sent1 + sent2))
+
+            vector1 = [0] * len(all_words)
+            vector2 = [0] * len(all_words)
+
+    # build the vector for the first sentence
+            for w in sent1:
+                if w in stopwords:
+                    continue
+                vector1[all_words.index(w)] += 1
+
+    # build the vector for the second sentence
+            for w in sent2:
+                if w in stopwords:
+                    continue
+                vector2[all_words.index(w)] += 1
+
+            return 1 - cosine_distance(vector1, vector2)
+
+
+        def build_similarity_matrix(sentences, stop_words):
+    # Create an empty similarity matrix
+            similarity_matrix = np.zeros((len(sentences), len(sentences)))
+
+            for idx1 in range(len(sentences)):
+                for idx2 in range(len(sentences)):
+                    if idx1 == idx2:  # ignore if both are same sentences
+                        continue
+                    similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+
+            return similarity_matrix
+
+        def generate_summary(ceviri, top_n=5):
+            stop_words = stopwords.words('turkish')
+            summarize_text = []
+
+    # Step 1 - Read text anc split it
+    #sentences = read_article(file_name)
+
+    # Step 2 - Generate Similary Martix across sentences
+            sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+    # Step 3 - Rank sentences in similarity martix
+            sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+            scores = nx.pagerank(sentence_similarity_graph)
+
+    # Step 4 - Sort the rank and pick top sentences
+            ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+            print("Indexes of top ranked_sentence order are ", ranked_sentence)
+
+            for i in range(top_n):
+                summarize_text.append(" ".join(ranked_sentence[i][1]))
+            
+            
+    # Step 5 - Offcourse,
+            print("Summarize Text: \n", ". ".join(summarize_text))
+            ozet_metin = str(summarize_text)
+# let's begin
+
+#read_article("deneme.txt")
+
+        generate_summary(ceviri, 5)
+        return render(request,"index.html",{"ozet":ozet_metin})
 
     return render(request,"index.html")
